36e4f1423263390454e3eb23ed2b8d8d124c695f
jsg3121 <xodm95@gmail.com>
2021-01-06 11:21:45 +0900

    FEAT(vue) : TypeScript버전, Vite사용 프로젝트 추가

---
+++ vue-vite/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div>asdfasdf</div>
+  TEST
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
